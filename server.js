d8e51df581b88dc98ecf709b3b8415e63cad5451
Jay Hernandez <ajuan_jose@hotmail.com>
2020-01-07 09:23:27 -0600

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'},
+    {name:'Panchito'},
+    {name:'Cheito'},
+    {name:'Chuchito'},
+    ]
+
 app.listen(8000, () => {
     console.log('App listening on port 8000!');
 });
+
+app.get('/users', (req, res) => {
+    console.log(req);
+    res.json({
+        succcess: true,
+        message: 'succcessfully got DA USERS, Coollio',
+        users: mockUserData,
+        ruta: req.route.path
+
+        
+        
+})});

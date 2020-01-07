b559a00c54a5b2bfa4443fe95e2e6aea3cf769cd
Jay Hernandez <ajuan_jose@hotmail.com>
2020-01-07 09:36:57 -0600

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -16,7 +18,7 @@ app.listen(8000, () => {
 app.get('/users', (req, res) => {
     console.log(req);
     res.json({
-        succcess: true,
+        success: true,
         message: 'succcessfully got DA USERS, Coollio',
         users: mockUserData,
         ruta: req.route.path
@@ -32,4 +34,29 @@ app.get('/users/:id', (req, res) => {
         message: "got one user",
         user: req.params.id
     })
+});
+
+app.post('/login', (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+     
+     if(username===mockUsername && password == mockPassword) {
+         res.json({
+             success: true,
+             message: "Password and username match",
+             token: "Here is your encrypted token"
+
+         })
+     }
+     else {
+         res.json({
+             success: false,
+             message: "la cagaste"
+         })
+     }
+
+    
 });
